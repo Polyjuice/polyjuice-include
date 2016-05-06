1e26d91c9706abb49ee0d5b5224b30ce8ce629b8
Tomek Wytrebowicz <tomalecpub@gmail.com>
2016-05-06 11:39:00 +0200

    Update mock server path in test helper for FF and IE

--- test/helpers/mock-htmlmerger.js
+++ test/helpers/mock-htmlmerger.js
@@ -3,7 +3,7 @@
     server.xhr.useFilters = true;
     server.xhr.addFilter(function (method, url) {
         //whenever the this returns true the request will not faked
-        return !url.match(/starcounter\//);
+        return !url.match(/sc\//);
     });
     server.respondWith('GET', /htmlmerger/, [200, {"Content-Type": "text/html"}, 'fake response']);
 }());
