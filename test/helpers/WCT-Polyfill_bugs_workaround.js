72541ace63de27bb0624a88ad1defdf7e1cdd71b
Tomek Wytrebowicz <tomalecpub@gmail.com>
2017-07-14 11:37:20 +0200

    Migrate to CE v1, Remove juicy-composition wrapper from light DOM
    https://github.com/Starcounter/starcounter-include/issues/39
    https://github.com/Starcounter/starcounter-include/issues/42
    Drop feature to automatically create slot elements and slot attributes.
    https://github.com/Starcounter/RebelsLounge/issues/107
    
    https://github.com/Starcounter/starcounter-include/issues/32
    Add default slot if there is no composition, for specific ones it should come from server (merged html)
    https://github.com/Juicy/juicy-composition/issues/4
    
    related
    https://github.com/Starcounter/RebelsLounge/issues/106
    https://github.com/Starcounter/starcounter-include/issues/26
    https://github.com/Starcounter/RebelsLounge/issues/78

--- test/helpers/WCT-Polyfill_bugs_workaround.js
+++ test/helpers/WCT-Polyfill_bugs_workaround.js
@@ -1,3 +1,3 @@
 HTMLElement.prototype.querySelector_template_for_buggy_polyfill = function querySelector_template_for_buggy_polyfill(){
-    return this.querySelector('juicy-composition').firstElementChild;
+    return this.firstElementChild;
 };
