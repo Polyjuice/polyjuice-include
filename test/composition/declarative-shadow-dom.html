Polyjuice/polyjuice-include

commit dc6c23ab651c1f1746f824eda383efc8c80ffd91

Problem: PR #60 introduced unintended regression - any call to _notifyPath resets an unsaved explicit composition created using CompositionEditor
Solution: change the _compositionChanged method to more clearly implement the complex process of selecting which composition to apply. Fix explicit composition caching so it does not get overwritten by fallback composition. Add a test

starcounter-include.html
@@ -210,35 +210,59 @@ version: 3.0.0-rc.6
                 this.template.model = this.partial
             }
         };
+
+        /**
+         * Stamps composition if needed
+         * @param  {String} compositionString HTML as a string to compare if a re-render is needed
+         * @param  {Function} callb Function that returns a composition to render
+         */
+        StarcounterIncludePrototype._renderCompositionChange = function(compositionString, callb) {
+            if (!this._forceLayoutChange && this._lastCompositionString == compositionString) {
+                return;
+            }
+            let composition = callb();
+            this.stampComposition(composition);
+            this._forceLayoutChange = false;
+            this._lastCompositionString = compositionString;
+        }
+
         /**
          * Handles change of the composition.
          * If not given explicitely, fetches one from provider.
-         * Stamps if needed.
+         * If not given in provider, fetches one from imported template's default composition.
+         * If imported template does not have a default composition, uses fallback composition.
+         * Warning: the explicit composition might come from https://github.com/Starcounter/starcounter-layout-html-editor/blob/17b21f729facd9a8dcd4241fb5c48cb71de11af5/starcounter-layout-html-editor.html#L253
          * @see .stampComposition
          * @param  {String} compositionString stringified HTML for new Shadow Root
          */
         StarcounterIncludePrototype._compositionChanged = function(compositionString) {
-            let composition;
-            if (this.partial && compositionString === undefined) {
-                const compositionProvider = findCompositionProvider(this.partial);
-                if (compositionProvider) {
-                    compositionString = compositionProvider.Composition$; //should always be string
-                    this.storedLayout = compositionString;
+            var that = this;
+
+            if (compositionString !== undefined) {
+                this._forceLayoutChange = true;
+                this._renderCompositionChange("explicit or fallback", function() {
+                    return that.stringToDocumentFragment(compositionString);
+                });
+            }
+            else {
+                const compositionProvider = this.partial && findCompositionProvider(this.partial);
+                if (compositionProvider && compositionProvider.Composition$) {
+                    this._renderCompositionChange(compositionProvider.Composition$, function() {
+                        that.storedLayout = compositionProvider.Composition$; //should always be string
+                        return that.stringToDocumentFragment(that.storedLayout);
+                    });
                 }
-            }
-            // do not change if there is one, it's same as before, and we are not forced
-            if (!compositionString) {
-                composition = this.defaultComposition && this.defaultComposition.cloneNode(true);
-                compositionString = this.defaultComposition;
-            } else {
-                // make clone to avoid direct binding
-                composition = this.stringToDocumentFragment(compositionString);
-            }
-            if (!compositionString || compositionString !== this._lastLayout || this._forceLayoutChange) {
-                this.stampComposition(composition);
-                this._forceLayoutChange = false;
-                this._lastLayout = compositionString;
-            }
+                else if (this.defaultComposition) {
+                    this._renderCompositionChange("imported template's default", function() {
+                        return that.defaultComposition.cloneNode(true);
+                    });
+                }
+                else {
+                    this._renderCompositionChange("explicit or fallback", function() {
+                        return undefined;
+                    });
+                }
+            }            
         };
         /**
          * Forward Polymer notification downwards from `<template is="dom-bind">`

test/partialAttribute/nested-html/stamped_from_polymer_template.html
@@ -204,6 +204,29 @@
                 });
             });
 
+            describe('after `dom-bind` changes the sub-partial property of a view with an explicit composition', function () {
+                var scope, include, newVal = "yeah", customComposition = "my custom composition";
+
+                beforeEach(function(done) {
+                    sinon.stub(importedTemplate, "_notifyPath");
+                    importedTemplate.addEventListener('stamping', function onceStamped(){
+                        importedTemplate.removeEventListener('stamping', onceStamped);                   
+                        include = container.querySelector('starcounter-include');
+                        include._compositionChanged(customComposition); //this is how starcounter-layout-html-editor uses it
+                        domBind.set('model.Page.scope1.doesItWork', newVal);
+                        done();
+                    });
+                });
+
+                it('should forward the notification to `imported-template`', function () {
+                    expect(importedTemplate._notifyPath).to.be.calledWith("model.scope1.doesItWork", newVal);
+                });
+
+                it('should be preserve the explicit composition', function () {
+                    expect(include.shadowRoot.innerHTML).to.be.equal(customComposition);
+                });
+            });
+
             describe('after `dom-bind`` changed the sub-partial property (just one scope) to new one with thethat results with the same merged Html', function () {
                 var changedScopeWithSameHtml;
                 beforeEach(function(done) {

commit be253847b17e8d3fd3bf541bb0746540de799a6c

Problem: commit dc6c23a introduced a bug - when views are switched and have a default composition, the new default composition is ignored
Solution: use the default composition instance as the comparison subject instead of a static string. Fix the test that was supposed to catch that but did not. Convert tabs to spaces in fixtures

sc/template_w_declarative-shadow-dom-alternative.html
@@ -1,7 +1,7 @@
 <template>
-    <h1 slot="my-slot">Template included</h1>
+    <h1 slot="my-slot">Alternative template included</h1>
     <template is="declarative-shadow-dom">
-        <h2>Custom Shadow DOM composition</h2>
+        <h2>Custom Shadow DOM alternative composition</h2>
         <slot name="my-slot"></slot>
     </template>
 </template>

starcounter-include.html
@@ -211,17 +211,17 @@ version: 3.0.0-rc.6
 
         /**
          * Stamps composition if needed
-         * @param  {String} compositionString HTML as a string to compare if a re-render is needed
+         * @param  {String|DocumentFragment} compositionRef reference to the evaluated composition
          * @param  {Function} callb Function that returns a composition to render
          */
-        StarcounterIncludePrototype._renderCompositionChange = function(compositionString, callb) {
-            if (!this._forceLayoutChange && this._lastCompositionString == compositionString) {
+        StarcounterIncludePrototype._renderCompositionChange = function(compositionRef, callb) {
+            if (!this._forceLayoutChange && this._lastCompositionRef == compositionRef) {
                 return;
             }
             let composition = callb();
             this.stampComposition(composition);
             this._forceLayoutChange = false;
-            this._lastCompositionString = compositionString;
+            this._lastCompositionRef = compositionRef;
         }
 
         /**
@@ -253,7 +253,7 @@ version: 3.0.0-rc.6
                     });
                 }
                 else if (this.defaultComposition) {
-                    this._renderCompositionChange("imported template's default", function() {
+                    this._renderCompositionChange(this.defaultComposition, function() {
                         return that.defaultComposition.cloneNode(true);
                     });
                 }

test/composition/declarative-shadow-dom.html
@@ -32,11 +32,19 @@
     </test-fixture>
     <template id="reference-composition">
         <h2>Custom Shadow DOM composition</h2>
-		<slot name="my-slot"></slot>
+        <slot name="my-slot"></slot>
+    </template>
+    <template id="reference-alternative-composition">
+        <h2>Custom Shadow DOM alternative composition</h2>
+        <slot name="my-slot"></slot>
     </template>
     <template id="reference-composition-v1-to-v0">
         <h2>Custom Shadow DOM composition</h2>
-		<content name="my-slot" select="[slot='my-slot']"></content>
+        <content name="my-slot" select="[slot='my-slot']"></content>
+    </template>
+    <template id="reference-alternative-composition-v1-to-v0">
+        <h2>Custom Shadow DOM alternative composition</h2>
+        <content name="my-slot" select="[slot='my-slot']"></content>
     </template>
 
 
@@ -44,14 +52,15 @@
     describe('starcounter-include when imports a template that contains <code>template is="declarative-shadow-dom"</code>', function () {
         const useShadowDOMV1 = Boolean(Element.prototype.attachShadow && Node.prototype.getRootNode);
         const REFERENCE_COMPOSITION = useShadowDOMV1 ? '#reference-composition' : '#reference-composition-v1-to-v0'
+        const REFERENCE_ALTERNATIVE_COMPOSITION = useShadowDOMV1 ? '#reference-alternative-composition' : '#reference-alternative-composition-v1-to-v0'
 
         var scInclude;
-        var partial = {
+        var alternativePartial = {
             "CompositionProvider": {
                 "PartialId": "given PartialId"
             },
             "App": {
-                "Html": "template_w_declarative-shadow-dom.html",
+                "Html": "template_w_declarative-shadow-dom-alternative.html",
                 "doesItWork": "works!"
             }
         };
@@ -71,13 +80,13 @@
         });
         describe('after partial property is replaced with the partial that also does contain default composition', function () {
             beforeEach(function (done) {
-                scInclude.partial = JSON.parse(JSON.stringify(partial));
+                scInclude.partial = JSON.parse(JSON.stringify(alternativePartial));
                 setTimeout(done, 500);
             });
             it('should use it as it\'s composition', function () {
                 expect(scInclude.shadowRoot).to.be.not.null;
                 expect(scInclude.shadowRoot.innerHTML.trim()).to.be
-                    .equal(document.querySelector(REFERENCE_COMPOSITION).innerHTML.trim());
+                    .equal(document.querySelector(REFERENCE_ALTERNATIVE_COMPOSITION).innerHTML.trim());
             });
         });
         describe('after partial property is replaced with itself', function () {