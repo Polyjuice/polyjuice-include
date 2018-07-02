bae65ff5ebd48a7ab8d60820a51cf09e6554d8e7
Tomek Wytrębowicz <tomalecpub@gmail.com>
2018-07-02 14:20:24 +0200

    Expose presentation type used & guessed by the element,
    as suggested at https://github.com/Starcounter/starcounter-include/pull/103#discussion_r198758589

--- test/helpers/composition-fixtures.js
+++ test/helpers/composition-fixtures.js
@@ -2,11 +2,11 @@ const isWebkit = navigator.vendor && navigator.vendor.indexOf("Apple") > -1;
 const useShadowDOMV1 = Boolean(Element.prototype.attachShadow && Node.prototype.getRootNode);
 
 const REFERENCE_CUSTOM = useShadowDOMV1 ? `customComposition!<slot></slot>` : `customComposition!<content select=":not([slot]),[slot=\'\']"></content>`;
-const REFERENCE_DEFAULT = useShadowDOMV1 ?  `<h2>Custom Shadow DOM composition</h2>
-        <slot name="my-slot"></slot>` : `<h2>Custom Shadow DOM composition</h2>
+const REFERENCE_DEFAULT = useShadowDOMV1 ?  `<h2>Default Shadow DOM presentation</h2>
+        <slot name="my-slot"></slot>` : `<h2>Default Shadow DOM presentation</h2>
         <content select="[slot=\'my-slot\']" name="my-slot"></content>`;
-const REFERENCE_HOST = useShadowDOMV1 ?  `<h2>Host Shadow DOM composition</h2>
-        <slot></slot>` : `<h2>Host Shadow DOM composition</h2>
+const REFERENCE_HOST = useShadowDOMV1 ?  `<h2>Parent Shadow DOM composition</h2>
+        <slot></slot>` : `<h2>Parent Shadow DOM composition</h2>
         <content></content>`;
 const REFERENCE_FALLBACK = useShadowDOMV1 ? `<style>:host{display:block;}</style><slot></slot>` : `<style>:host{display:block;}</style><content select=":not([slot]),[slot=\'\']"></content>`;
 
