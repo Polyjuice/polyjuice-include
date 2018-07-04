872b6dec42742232bcd6338ee72f647a8dc6b6e2
Tomek Wytrębowicz <tomalecpub@gmail.com>
2018-07-04 12:22:19 +0200

    Rename reference presentation to `REFERENCE_PARENT` in tests,
    to keep naming consistent,
    as suggested at https://github.com/Starcounter/starcounter-include/pull/103#discussion_r199906994

--- test/helpers/composition-fixtures.js
+++ test/helpers/composition-fixtures.js
@@ -5,7 +5,7 @@ const REFERENCE_CUSTOM = useShadowDOMV1 ? `customComposition!<slot></slot>` : `c
 const REFERENCE_DEFAULT = useShadowDOMV1 ?  `<h2>Default Shadow DOM presentation</h2>
         <slot name="my-slot"></slot>` : `<h2>Default Shadow DOM presentation</h2>
         <content select="[slot=\'my-slot\']" name="my-slot"></content>`;
-const REFERENCE_HOST = useShadowDOMV1 ?  `<h2>Parent Shadow DOM composition</h2>
+const REFERENCE_PARENT = useShadowDOMV1 ?  `<h2>Parent Shadow DOM composition</h2>
         <slot></slot>` : `<h2>Parent Shadow DOM composition</h2>
         <content></content>`;
 const REFERENCE_FALLBACK = useShadowDOMV1 ? `<style>:host{display:block;}</style><slot></slot>` : `<style>:host{display:block;}</style><content select=":not([slot]),[slot=\'\']"></content>`;
