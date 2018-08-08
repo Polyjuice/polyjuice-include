ec45f43d9bf1cbb756717abb7ddcfbb9f237f8cd
Omar Alshaker <omar@omaralshaker.com>
2018-08-08 11:22:53 +0200

    Add package-log.json to gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -28,10 +28,10 @@ module.exports = function(grunt) {
 
         bump: {
           options: {
-            files: ['package.json', 'starcounter-include.html'],
+            files: ['package.json', 'package-lock.json', 'starcounter-include.html'],
             commit: true,
             commitMessage: '%VERSION%',
-            commitFiles: ['package.json', 'starcounter-include.html'],
+            commitFiles: ['package.json', 'package-lock.json', 'starcounter-include.html'],
             createTag: true,
             tagName: '%VERSION%',
             tagMessage: 'Version %VERSION%',
